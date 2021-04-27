7de0970f4bddf005e88c3e07acbde4ccb271d1d0
Agnieszka Brown <abrown2012@icloud.com>
2021-04-26 23:32:49 -0400

    commit tree f754eb23c379aa00c4eb3a638c38eab3ab8f9638

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    return drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    return drivers.shift();
+}
+
+function appendDriver(name) {
+    let new_driver;
+    return new_driver = [...drivers, name];
+}
+
+function prependDriver(name) {
+    let new_driver;
+    return new_driver = [name, ...drivers];
+}
+
+function removeLastDriver() {
+    let new_driver =  drivers.slice(0, -1);
+    return new_driver
+}
+
+function removeFirstDriver() {
+    let new_driver = drivers.slice(1)
+    return new_driver
+}
